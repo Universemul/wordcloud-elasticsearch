8dd47161b9a4510b9f9c7da95b623f518215e13a
David Barthelemy <davidbarthelemy28@gmail.com>
2021-07-19 23:34:16 +0200

    new cities file

--- import_data.py
+++ import_data.py
@@ -2,9 +2,10 @@ import json
 import uuid
 import csv
 
-from engine import ElasticSearchEngine
 from models import Message, MeetingPoint
-from es_helpers import delete_wordcloud_index, delete_meetingpoint_index, create_wordcloud_index, create_meetingpoint_index, MEETING_POINTS_INDEX, WORDCLOUD_INDEX, bulk_index
+from es_helpers import delete_wordcloud_index, delete_meetingpoint_index, create_wordcloud_index, \
+    create_meetingpoint_index, MEETING_POINTS_INDEX, WORDCLOUD_INDEX, bulk_index
+
 
 def wordcloud():
     _file = "samples/reviews_Amazon_Instant_Video_5.json"
@@ -13,8 +14,8 @@ def wordcloud():
     documents = []
     with open(_file, "r") as f:
         lines = f.readlines()
-        for l in lines:
-            tmp = json.loads(l)
+        for line in lines:
+            tmp = json.loads(line)
             msg = Message()
             msg.message = tmp["reviewText"]
             msg.sender = tmp.get("reviewerName", "")
@@ -25,14 +26,14 @@ def wordcloud():
                 '_source': msg.to_dict()
             })
     if not documents:
-       print("FAIL INDEXATION")
-       exit(-1)
+        print("FAIL INDEXATION")
+        exit(-1)
     bulk_index(documents)
     print("INDEXATION OK")
 
+
 def meetingpoints():
-    _file = "samples/mps_fr.csv"
-    es = ElasticSearchEngine()
+    _file = "samples/cities.csv"
     delete_meetingpoint_index()
     create_meetingpoint_index()
     documents = []
@@ -40,24 +41,26 @@ def meetingpoints():
         csv_reader = csv.DictReader(f)
         for row in csv_reader:
             item = MeetingPoint()
-            item.name = row['NAME']
-            item.name_suggest = row['NAME']
-            item.name_prefix = row['NAME']
-            item.name_ngram = row['NAME']
-            item.id = row['ID']
+            item.name = row['city']
+            item.name_suggest = row['city']
+            item.name_prefix = row['city']
+            item.name_ngram = row['city']
+            item.id = row['id']
             documents.append({
                 '_id': str(uuid.uuid4()),
                 '_index': MEETING_POINTS_INDEX,
                 '_source': item.to_dict()
             })
     if not documents:
-       print("FAIL INDEXATION")
-       exit(-1)
+        print("FAIL INDEXATION")
+        exit(-1)
     bulk_index(documents)
     print("INDEXATION OK")
 
+
 def run():
-    #wordcloud()
+    wordcloud()
     meetingpoints()
+
 
 run()
--- launch.sh
+++ launch.sh
@@ -1,2 +1,3 @@
 export FLASK_APP=wordcloud
+export FLASK_ENV=development
 flask run
